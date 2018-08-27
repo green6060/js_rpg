c827673d70b9895a2a1532c1ef0ec470d944b8e2
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-27 13:29:16 -0600

    Finished Player Creation Page, front end, and passing values to main.js
    Finished Player Creation Page, front end, and passing values to main.js

--- 4_resources/js/main.js
+++ 4_resources/js/main.js
@@ -1,5 +1,10 @@
 $("#playerCreateBtn").click(function(){
   var playerFirstName = $("input[name=firstName]").val();
   var playerTitle = $("input[name=title]").val();
+  var playerGender = $("input[name=sex]:checked").val();
+  var playerClass= $( "#playerClass option:selected").val();
+  var playerRace = $( "#playerRace option:selected").val();
+  var playerDiety = $( "#playerDiety option:selected").val();
   debugger;
+
 });
